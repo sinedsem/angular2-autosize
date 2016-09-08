a9915bd729b5c5384810c58997fa264c0478bdef
Denis <sinedsem@gmail.com>
2016-09-08 17:54:53 +0300

    fix bug when page couldn't be scrolled to the end due to collapsing and then restoring textarea

--- src/autosize.directive.ts
+++ src/autosize.directive.ts
@@ -5,18 +5,31 @@ import { ElementRef, HostListener, Directive} from '@angular/core';
 })
 
 export class Autosize {
- @HostListener('input',['$event.target'])
-  onInput(textArea: HTMLTextAreaElement): void {
-    this.adjust();
-  }
-  constructor(public element: ElementRef){
-  }
-  ngOnInit(): void{
-    this.adjust();
-  }
-  adjust(): void{
-    this.element.nativeElement.style.overflow = 'hidden';
-    this.element.nativeElement.style.height = 'auto';
-    this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
-  }
+    @HostListener('input', ['$event.target'])
+    onInput(textArea: HTMLTextAreaElement): void {
+        this.adjust();
+    }
+
+    constructor(public element: ElementRef) {
+    }
+
+    ngAfterContentChecked(): void {
+        this.adjust();
+    }
+
+    adjust(): void {
+        var elem = this.element.nativeElement; //let's shorten lines a little
+
+        // save current margin-bottom
+        let style = elem.currentStyle || window.getComputedStyle(elem);
+        let marginBottom = style.marginBottom ? parseInt(style.marginBottom) : 0;
+
+        elem.style.overflow = 'hidden';
+
+        // add current height in order not to lose window scroll position due to textarea collapse
+        elem.style.marginBottom = marginBottom + elem.offsetHeight + "px";
+        elem.style.height = 'auto';
+        elem.style.height = elem.scrollHeight + 3 + "px"; // +3 added due to Mirefox cutting text a little
+        elem.style.marginBottom = marginBottom + "px"; // restore original margin
+    }
 }
